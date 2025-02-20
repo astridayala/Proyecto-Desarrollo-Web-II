de08cde1fdfd476f74eaa0e2c3ae6cbb874f574f
astridayala <astriayala06@gmail.com>
2025-02-20 16:27:29 -0600

    arreglando errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Profile from "./pages/Profile";
 import { HeaderCliente } from './components/HeaderClient';
 import { Footer } from './components/Footer';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <HeaderCliente />
-        <Footer />
-      </Router>
-    </AuthProvider>
+    <Router>
+      <HeaderCliente />
+      <Footer />
+    </Router>
   );
 }
 
