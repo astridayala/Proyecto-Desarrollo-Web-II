a38be632660fe0cf421cf3c19398bd22a0a412da
danielaguardad0 <danielamguatdado@gmail.com>
2025-02-20 19:26:44 -0600

    nav admin general

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HeaderCliente } from './components/HeaderClient';
 import { Footer } from './components/Footer';
+import { HeaderGeneralAdmin } from "./components/HeaderGeneralAdmin";
 
 function App() {
   return (
     <Router>
-      <HeaderCliente />
+      <HeaderGeneralAdmin />
       <Footer />
     </Router>
   );
