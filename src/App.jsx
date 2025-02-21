1eefe4e3be8990ea753506505c23d7f06a0df8b0
danielaguardad0 <danielamguatdado@gmail.com>
2025-02-20 19:33:00 -0600

    nav admin ofertante

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HeaderCliente } from './components/HeaderClient';
 import { Footer } from './components/Footer';
 import { HeaderGeneralAdmin } from "./components/HeaderGeneralAdmin";
+import { HeaderAdminOfertante } from "./components/HeaderAdminOfertante";
 
 function App() {
   return (
     <Router>
-      <HeaderGeneralAdmin />
+      <HeaderAdminOfertante />
       <Footer />
     </Router>
   );
