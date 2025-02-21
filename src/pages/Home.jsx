75b80f5f2a9e50f2575b7f7a841878183e495db9
leviuuuwwu <20235838@esen.edu.sv>
2025-02-21 16:59:12 -0600

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";  // 🔹 Importar Link
+import { Link } from "react-router-dom";
 import { HeaderGeneral } from "../components/HeaderGeneral";
 import { db } from "../firebase/config";
 import { collection, getDocs } from "firebase/firestore";
@@ -23,18 +23,21 @@ const Home = () => {
   return (
     <div className="min-h-screen bg-gray-100">
       <HeaderGeneral />
-      <main className="container mx-auto p-5 pt-24">
-        <h1 className="text-3xl font-semibold text-gray-800 mb-6">Ofertas Disponibles</h1>
+      <main className="container mx-auto p-5 pt-24 pb-10"> 
+        <h1 className="text-3xl font-semibold text-gray-800 mb-4">Ofertas Disponibles</h1> 
 
         {ofertas.length === 0 ? (
           <p className="text-center text-gray-600">No hay ofertas disponibles.</p>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
             {ofertas.map((oferta) => (
               <div key={oferta.id} className="bg-white shadow-lg rounded-lg p-4">
                 <h2 className="text-lg font-semibold">{oferta.titulo}</h2>
-                <p className="text-gray-700">Precio regular: <span className="line-through">${oferta.precioRegular}</span></p>
+                <p className="text-gray-700">
+                  Precio regular: <span className="line-through">${oferta.precioRegular}</span>
+                </p>
                 <p className="text-green-600 font-bold">Precio oferta: ${oferta.precioOferta}</p>
+                <p className="text-gray-700">Descripción: {oferta.descripcion}</p> 
                 <Link 
                   to={`/oferta/${oferta.id}`} 
                   className="block mt-3 bg-blue-500 text-white text-center py-2 rounded-lg hover:bg-blue-600"
