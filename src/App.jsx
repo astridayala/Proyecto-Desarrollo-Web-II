e463b3c67ed573262dfc67ef1cf7d4609350f8cb
danielaguardad0 <danielamguatdado@gmail.com>
2025-02-21 08:23:24 -0600

    new page to empleados

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Footer } from './components/Footer';
 import GestionOfertas from "./pages/GestionOfertas";
+import GestionEmpleados from "./pages/GestioEmpleados";
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/gestion-ofertas" element={<GestionOfertas />} />
+      <Route path="/gestion-empleados" element={<GestionEmpleados />} />
       </Routes>
       <Footer />
     </Router>
